29c3f39e7f936cb9d066245bb639aefc95c5a37c
Theeksha <139316266+Theeksha12@users.noreply.github.com>
2025-03-11 14:37:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('MenuApp', ['ui.router'])
-  .service('MenuDataService', ['$http', function ($http) {
-    var service = this;
+    angular.module('MenuApp', ['ngRoute'])
+    .config(RoutesConfig);
 
-    service.getAllCategories = function () {
-      return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/categories.json')
-        .then(response => response.data);
-    };
-
-    service.getItemsForCategory = function (categoryShortName) {
-      return $http.get(`https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/${categoryShortName}.json`)
-        .then(response => response.data.menu_items);
-    };
-  }])
-  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider
-      .state('home', {
-        url: '/',
-        template: '<h1>Welcome to our Restaurant</h1><a ui-sref="categories">View Categories</a>'
-      })
-      .state('categories', {
-        url: '/categories',
-        template: '<ul><li ng-repeat="category in catCtrl.categories"><a ui-sref="items({ categoryShortName: category.short_name })">{{ category.name }}</a></li></ul>',
-        controller: ['MenuDataService', function (MenuDataService) {
-          var catCtrl = this;
-          MenuDataService.getAllCategories().then(data => catCtrl.categories = data);
-        }],
-        controllerAs: 'catCtrl'
-      })
-      .state('items', {
-        url: '/items/{categoryShortName}',
-        template: '<ul><li ng-repeat="item in itemCtrl.items">{{ item.name }} - {{ item.description }}</li></ul>',
-        controller: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
-          var itemCtrl = this;
-          MenuDataService.getItemsForCategory($stateParams.categoryShortName).then(data => itemCtrl.items = data);
-        }],
-        controllerAs: 'itemCtrl'
-      });
-  }]);
-
+    RoutesConfig.$inject = ['$routeProvider'];
+    function RoutesConfig($routeProvider) {
+        $routeProvider
+            .when('/signup', {
+                templateUrl: 'signup.html',
+                controller: 'SignupController as signupCtrl'
+            })
+            .when('/myinfo', {
+                templateUrl: 'myinfo.html',
+                controller: 'MyInfoController as myinfoCtrl'
+            })
+            .otherwise({
+                redirectTo: '/signup'
+            });
+    }
 })();
